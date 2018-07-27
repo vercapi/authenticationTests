c6918805376d3c5104061525c414ec66925c9f3e
Pieter <pieterv.sorano@gmail.com>
2018-07-27 10:29:21 +0200

    Changed the authentication provider to an identity asserter
    This allows us to pass tokens to assert identity and there is no need
    to use a username and password.
    
    - Added SimpleCallbackHandler that supplies a username. This is the
      identity passed to weblogic.
    - OAuth2AsserterTokenTypes this holds the only valid token type
      OAUthToken for this type.
     - OAuthEndpointservlet no longers logs in via login but with
      assertIdentity so that the new Identity Asserter methods in
      Auth2authenticationporviderimpl get called whenever the servlet is
      called.
    - OAuthAuthentication.xml is changed in order for the Authentication
      Provider to act as an Identity Asserter.

---
+++ OAuthProvider/src/main/java/be/vercapi/oauth2provider/SimpleCallbackHandler.java
@@ -0,0 +1,34 @@
+package be.vercapi.oauth2provider;
+
+import java.io.IOException;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+public class SimpleCallbackHandler implements  CallbackHandler{
+
+    private String username;
+
+    public SimpleCallbackHandler(String username) {
+        this.username = username;
+    }
+
+	@Override
+	public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+    for (int i = 0; i < callbacks.length; i++) {
+ 
+      Callback callback = callbacks[i];
+ 
+      // we only handle NameCallbacks
+      if (!(callback instanceof NameCallback)) {
+        throw new UnsupportedCallbackException(callback, "Unrecognized Callback");
+      }
+ 
+      // send the user name to the name callback:
+      NameCallback nameCallback = (NapmeCallback)callback;
+      nameCallback.setName(username);
+    }
+	}
+}
